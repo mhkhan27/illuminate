843d9372eb30281d888a9c16efab98b82570c3a8
Md. Mehedi Hasan Khan <mhkhan27@users.noreply.github.com>
2020-11-09 14:09:17 +0600

    add quick survey function

---
+++ R/quick_survey.R
@@ -0,0 +1,57 @@
+#' Quick Surveys
+#' @param df data set
+#' @param consent_column_name column where surevy consent status is stored (default is X_uuid)
+#' @param uuid_col_name  column contining unique identifier
+#' @param start_question_node start question node from where calculation for time duration will start (audit node is excluded)
+#' @param end_question_node end question node to where calculation for time duration will end (audit node is excluded)
+#' @param min_allowable_duration Minumum allowable duration
+#' @param audit_zipfile Audit zipe file location
+#' @param path_unzip Path for unzip
+#' @param copy_zip_to
+#' @return Quick survey list
+#' @export
+#'
+
+quick_survey<-function(df,
+                       consent_column_name,
+                       uuid_col_name = "X_uuid",
+                       start_question_node,
+                       end_question_node,
+                       min_allowable_duration,
+                       audit_zipfile,
+                       path_unzip,
+                       copy_zip_to){
+
+  audit_yes<-butteR::load_audit(data=df,
+                                path.to.zip = audit_zipfile,
+                                path.to.unzip = path_unzip,
+                                copy.zip = TRUE,
+                                path.to.copy.zip = copy_zip_to,
+                                delete.unzipped=TRUE
+  )
+
+
+  dfl<-list()
+  for (i in 1: length(audit_yes)){
+    d<-audit_yes[[i]]
+
+    d$node<-gsub("\\[1]","",d$node)
+
+    start_question <- d %>% filter(node==paste0(audit_node,start_question_node)& !is.na(event)) %>%
+      select(end)
+    start_question<-min(start_question$end)
+    end_question<-d %>% filter(node==paste0(audit_node,end_question_node)& !is.na(node)) %>%
+      select(end)
+
+    end_question<-max(end_question$end)
+    duration_ms<-end_question-start_question
+    duration_secs<-duration_ms/1000
+    duration_minutes<- round(duration_secs/60,1)
+    dfl[[i]]<-data.frame(uuid=names(audit_yes)[i],duration_ms=duration_ms,durations_secs=duration_secs,duration_minutes= duration_minutes)
+  }
+
+  duration_df2<-do.call("rbind", dfl)
+  quick_survey_df <- duration_df2 %>% dplyr::filter(duration_minutes > min_allowable_duration)
+  quick_survey_df <- quick_survey_df %>% dplyr::select(-c("duration_ms","durations_secs"))
+  return(quick_survey_df)
+}
