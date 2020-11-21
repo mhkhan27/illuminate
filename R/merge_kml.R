81ea49c292ee4418c72635f222a3713f034b8954
Md. Mehedi Hasan Khan <mhkhan27@users.noreply.github.com>
2020-11-21 13:07:18 +0600

    fix merger_kml

--- R/merge_kml.R
+++ R/merge_kml.R
@@ -1,16 +1,16 @@
 #' Use to merge KMLs
 #' @param input_folder Input folder from where KMLs will be merged.
-#' @param output_folder Output folder in where merged KML will be saved.
 #' @export
 
 
-merge_kml <- function(input_folder,output_folder){
+merge_kml <- function(input_folder){
 
 fl <- list.files(input_folder,full.names = T)
 kml <- list()
 for (i in  1:length(fl)){
   kml[[i]] <- st_read(fl[i])}
 a<- do.call("rbind",kml)
-plotKML(sf::as_Spatial(a), paste0(output_folder,"merged_all"))}
+return(a)
+}
 
 
