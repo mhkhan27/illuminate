ecce1d4168eaf869799fba1319e77fe31e4e4790
mhkhan27 <plannermehedi@gmail.com>
2021-06-04 17:31:26 +0300

    add documnets

--- R/remove_multiple_choice_from_data.R
+++ R/remove_multiple_choice_from_data.R
@@ -1,10 +1,11 @@
 #' Generate dataset for butteR::survey_collaps()
 #' @param df The data set
 #' @param kobo_servey_sheet The defult (NUll) will create additional table for multiple choice questions
+#' @param remove_multiple_choice_concat it is a logical parameter. Ture will remove the aggregated columns (for multiple choice question) from the dataset
 #' @export
 
 
-fix_data_type_frm_kobo <- function(df,kobo_servey_sheet){
+fix_data_type_frm_kobo <- function(df,kobo_servey_sheet,remove_multiple_choice_concat = T){
 
   select_mutiple_from_kobo <- kobo_servey_sheet %>% dplyr::filter(str_detect(type,"select_multiple"))
   integer_from_kobo <- kobo_servey_sheet %>% dplyr::filter(str_detect(type,"integer"))
@@ -25,7 +26,11 @@ fix_data_type_frm_kobo <- function(df,kobo_servey_sheet){
 
   select_mutiple_from_kobo_cols<- select_mutiple_from_kobo_cols[select_mutiple_from_kobo_cols %in% names(df)]
 
+  if(remove_multiple_choice_concat == T){
+
   df <- df %>% dplyr::select(-select_mutiple_from_kobo_cols)
+  }
+
 
   for(i in select_mutiple_from_kobo_cols ){
     df_cols_to_logical <- df %>% select(starts_with(paste0(i,"."))) %>% names()
