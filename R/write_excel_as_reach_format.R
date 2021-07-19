a909de5ff42aa0335cc199d789c8a8e61879e1d1
mhkhan27 <plannermehedi@gmail.com>
2021-07-19 13:15:01 +0300

    update export_as_reach_format

--- R/write_excel_as_reach_format.R
+++ R/write_excel_as_reach_format.R
@@ -1,4 +1,4 @@
-write_excel_as_reach_format <- function(write_list,output_path){
+write_excel_as_reach_format <- function(write_list,output_path,cols_for_color = NULL){
 
 
   headerStyle <- createStyle(fontSize = 12,
@@ -42,57 +42,36 @@ write_excel_as_reach_format <- function(write_list,output_path){
     setColWidths(wb, i, cols = 1:ncol(dataset), widths = 25)
     setRowHeights(wb, i, 1, 20)
 
+    if(!is.null(cols_for_color)){
+      u = unique(dataset[[cols_for_color]])
 
+      for(x in u){
+        y = which(dataset[[cols_for_color]] == x)
+
+        random.color <- randomColor(1, luminosity = "light")
+
+        style <- createStyle(fgFill=random.color,
+                             fontSize = 11,
+                             fontName = "Arial Narrow",
+                             border = "TopBottomLeftRight ",
+                             borderColour = "#4F81BD",
+                             valign = "center",
+                             halign = "left")
+
+
+        addStyle(wb, sheet = i, style, rows = y+1, cols = 1:ncol(dataset), gridExpand = TRUE)
+
+
+
+      }
+
+
+    }
 
   }
+
 
   saveWorkbook(wb, file = output_path, overwrite = TRUE)
 
 }
-
-
-
-
-
-
-
-
-
-calculateDifferences <- function(data, tool.survey){
-  # 1) convert all columns to character
-  data <- mutate_all(data, as.character)
-  # 2) remove columns that are naturally different in each survey
-  cols <- data.frame(column=colnames(data)) %>%
-    left_join(dplyr::select(tool.survey, name, type), by=c("column"="name")) %>%
-    filter(!(type %in% c("date", "start", "end",
-                         "audit", "note", "calculate", "geopoint")) &
-             !str_starts(column, "_") &
-             !str_detect(column, "/"))
-  data <- data[, all_of(cols$column)]
-  # 3) convert NA to "NA" and all columns to factor
-  data[is.na(data)] <- "NA"
-  data <- data %>% mutate_if(is.character, factor)
-  # 4) calculate gower distance
-  gower_dist <- daisy(data, metric="gower", warnBin=F, warnAsym=F, warnConst=F)
-  gower_mat <- as.matrix(gower_dist)
-  # 5) convert distance to number of differences and determine closest matching survey
-  r <- c()
-  for (i in 1:nrow(data)) r <- c(r, sort(gower_mat[i,]*ncol(data))[2])
-  # 6) store results
-  data[["n.col.not.NA"]] <- rowSums(data!="NA")
-  data[["survey.id"]] <- 1:dim(data)[1]
-  data[["most.similar.survey"]] <- names(r)
-  data[["number.different.columns"]] <- as.numeric(r)
-  data <- data %>% arrange(number.different.columns, survey.id)
-  return(data)
-}
-
-
-
-
-
-
-
-
-
 
