a85edb2fde5504ab2cd82807fd8c5de257fbba1b
Md Mehedi Hasan KHAN <plannermehedi@gamil.com>
2022-10-02 00:11:44 +0300

    fix bug in weighted t test

--- R/weighted statistical test.R
+++ R/weighted statistical test.R
@@ -143,7 +143,14 @@ weighted_t_test <- function(data,binary_variable,non_binary_variable,strata,surv
   for(i in non_binary_variable){
 
      out_liers_independent_variable <- boxplot.stats(data[[i]])$out
+     if(length(out_liers_independent_variable) > 0){
      df <- data %>% filter(!data[[i]] %in% out_liers_independent_variable)
+     }
+
+     if(length(out_liers_independent_variable) == 0){
+       df <- data
+     }
+
      df <- df %>% filter(!is.na(df[[i]]))
      df <- df %>% filter(! is.infinite(df[[i]]))
 
